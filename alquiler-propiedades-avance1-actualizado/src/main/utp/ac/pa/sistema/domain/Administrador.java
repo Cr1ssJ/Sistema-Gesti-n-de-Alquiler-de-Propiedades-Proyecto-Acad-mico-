cbee3bc7d84d90ca60cf848421d892691c130a02
Cr1ssJ <cristianjime2026@gmail.com>
2025-10-24 02:48:43 -0500

    Delete alquiler-propiedades-avance1-actualizado directory

--- alquiler-propiedades-avance1-actualizado/src/main/utp/ac/pa/sistema/domain/Administrador.java
+++
@@ -1,7 +0,0 @@
-package utp.ac.pa.sistema.domain;
-
-public class Administrador extends Usuario {
-    public Administrador(String id, String nombreUsuario) {
-        super(id, nombreUsuario, "ADMIN");
-    }
-}
--- alquiler-propiedades-avance1-actualizado/src/main/utp/ac/pa/sistema/domain/GestorReportes.java
+++
@@ -1,6 +0,0 @@
-package utp.ac.pa.sistema.domain;
-
-public class GestorReportes {
-    public void generarReportePorUbicacion(){ /* implementación futura */ }
-    public void generarReportePorTipoPropiedad(){ /* implementación futura */ }
-}
